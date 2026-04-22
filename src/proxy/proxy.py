4e7fb72fa956806854b1c4c947f12e58b86b3cca
“Bachir <dqjfc@iup.edu>
2026-04-22 12:54:57 -0400

    Bachir's AWS SAM project initial commit

---
+++ src/proxy/proxy.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+import json
+
+s3 = boto3.client('s3')
+
+def proxy_handler(event, context):
+    # Log the event argument for debugging and for use in local development.
+    print(json.dumps(event))
+
+
+    # get the name of the bucket from the environment variable
+    bucket = os.environ('BUCKET_BUCKET_NAME')
+
+    #Fetch the file 
+    response = s3.get_object(Bucket=bucket, Key='index.html')
+    html_boday = response['Body'].read().decode('utf-8')
+
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/html"},
+        "body": html_boday
+    }
+
